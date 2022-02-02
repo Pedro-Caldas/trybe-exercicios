3dfd02a869545a02747d7014b55e349ff2c278ad
Pedro Caldas <pedronerislc@gmail.com>
2022-02-02 19:30:50 -0300

    Add task 2

--- fundamentos/bloco-04-introducao-a-java-script/dia-1-java-script-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-java-script/dia-1-java-script-primeiros-passos/script.js
@@ -1,21 +1,12 @@
 // Exercise with fifteen tasks
 
-// Task 1
+// Task 2
 
-let a = 4;
-let b = 2;
+const a = 3
+const b = 5
 
-let adicao = (a + b);
-console.log(adicao);
-
-let subtracao = (a - b);
-console.log(subtracao);
-
-let multiplicacao = (a * b);
-console.log(multiplicacao);
-
-let divisao = (a / b);
-console.log(divisao);
-
-let modulo = (a % b);
-console.log(modulo);
+if (a > b) {
+    console.log(a);
+} else {
+    console.log(b);
+}
