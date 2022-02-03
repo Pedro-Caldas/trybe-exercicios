3c996d65b3ec96206ff7aa3ef0a20690202f682b
Pedro Caldas <pedronerislc@gmail.com>
2022-02-03 10:02:39 -0300

    Add task 8

--- fundamentos/bloco-04-introducao-a-java-script/dia-1-java-script-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-java-script/dia-1-java-script-primeiros-passos/script.js
@@ -1,21 +1,13 @@
 // Exercise with fifteen tasks
 
-// Task 7
+// Task 8
 
-const grade = 64;
+const a = 1;
+const b = 3;
+const c = 5;
 
-if (grade >= 90 && grade <= 100) {
-    console.log("A")
-} else if (grade >= 80 && grade <= 89) {
-    console.log("B");
-} else if (grade >= 70 && grade <= 79) {
-    console.log("C");
-} else if (grade >= 60 && grade <= 69) {
-    console.log("D");
-} else if (grade >= 50 && grade <= 59) {
-    console.log("E");
-} else if (grade < 50) {
-    console.log("F");
+if ((a % 2 == 0) || (b % 2 == 0) || (c % 2 == 0)) {
+    console.log(true);
 } else {
-    console.log("error, grade invalid");
+    console.log(false);
 }
