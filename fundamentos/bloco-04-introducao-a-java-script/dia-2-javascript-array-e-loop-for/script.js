aec6e8ff40ecb1477368ff75cd63feba86c88ef5
Pedro Caldas <pedronerislc@gmail.com>
2022-02-03 15:50:28 -0300

    Add task 4

--- fundamentos/bloco-04-introducao-a-java-script/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-java-script/dia-2-javascript-array-e-loop-for/script.js
@@ -20,6 +20,19 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Task 3
 
+// let sum = 0;
+// let counter = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     sum = sum + numbers[index];
+//     counter = counter + 1;
+// }
+
+// let mean = (sum/counter);
+// console.log(mean);
+
+// Task 4
+
 let sum = 0;
 let counter = 0;
 
@@ -29,4 +42,10 @@ for (let index = 0; index < numbers.length; index += 1) {
 }
 
 let mean = (sum/counter);
-console.log(mean);
+
+if (mean > 20) {
+    console.log("Valor maior que 20");
+} else {
+    console.log("Valor menor que 20")
+}
+
