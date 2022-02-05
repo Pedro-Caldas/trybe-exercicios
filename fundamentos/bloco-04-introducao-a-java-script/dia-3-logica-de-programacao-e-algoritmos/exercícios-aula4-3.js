79dca9d1e4ba2838f712f336e7523e1881dd9c42
Pedro Caldas <pedronerislc@gmail.com>
2022-02-05 07:07:30 -0300

    Add exercise 2

--- fundamentos/bloco-04-introducao-a-java-script/dia-3-logica-de-programacao-e-algoritmos/exercícios-aula4-3.js
+++ fundamentos/bloco-04-introducao-a-java-script/dia-3-logica-de-programacao-e-algoritmos/exercícios-aula4-3.js
@@ -1,15 +1,26 @@
 // Ex. 1
 
-let numero = 3;
-let arrayInteiros = [];
-let fatorial = 1;
+// let numero = 3;
+// let arrayInteiros = [];
+// let fatorial = 1;
 
-for (let index = 1; index < numero + 1; index += 1) {
-    arrayInteiros.push(index);
+// for (let index = 1; index < numero + 1; index += 1) {
+//     arrayInteiros.push(index);
+// }
+
+// for (let index2 = 0; index2 < arrayInteiros.length; index2 += 1) {
+//     fatorial = fatorial * arrayInteiros[index2];
+// }
+
+// console.log(fatorial);
+
+// Ex. 2
+
+let word = "pedro";
+let backWord = " ";
+
+for (let index = word.length - 1; index >= 0; index -= 1){
+    backWord = backWord + word[index];
 }
 
-for (let index2 = 0; index2 < arrayInteiros.length; index2 += 1) {
-    fatorial = fatorial * arrayInteiros[index2];
-}
-
-console.log(fatorial);
+console.log(backWord);
