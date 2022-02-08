beb54a87115b522535bb4a65cb361f9f047eda8e
Pedro Caldas <pedronerislc@gmail.com>
2022-02-08 14:03:39 -0300

    Add part 2

--- fundamentos/bloco-04-introducao-a-java-script/dia-4-objetos-e-funcoes/exercicios-4-4.js
+++ fundamentos/bloco-04-introducao-a-java-script/dia-4-objetos-e-funcoes/exercicios-4-4.js
@@ -1,4 +1,4 @@
-// Part 1 - 8 exercises
+// Part 1 - objetcs and for/in
 
 // let info = {
 //     personagem: 'Margarida',
@@ -77,3 +77,99 @@
 // Ex. 8
 
 // console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos")
+
+
+// ----
+
+// Part 2 - functions
+
+// Ex. 1
+
+// Fonte para função de reverter palavra: https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
+
+// function palindrome(word) {
+//     reverseWord = word.split("").reverse().join("");
+//     if (reverseWord === word) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(palindrome("arara"))
+
+// Ex. 2
+
+// function retornaMaiorIndice(numeros) {
+//     let maiorIndice = 0;
+//     for (let indice in numeros) {
+//         if (numeros[maiorIndice] < numeros[indice]) {
+//             maiorIndice = indice
+//         }
+//     }
+//     return maiorIndice;
+// }
+// console.log(retornaMaiorIndice([2, 3, 6, 7, 10, 1]))
+
+// Ex. 3
+
+// function returnLowest(numbers) {
+//     let lowestIndex = 0;
+//     for (let index in numbers) {
+//         if (numbers[lowestIndex] >= numbers[index]) {
+//             lowestIndex = index;
+//         }
+//     }
+//     return lowestIndex;
+// }
+// console.log(returnLowest([2, 4, 6, 7, 10, 0, -3]))
+
+// Ex. 4
+
+// function longestName(names) {
+//     let longest = " ";
+//     for (let name in names) {
+//         if (longest.length < names[name].length) {
+//             longest = names[name];
+//         }
+//     }
+//     return longest
+// }
+// console.log(longestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']))
+
+// Ex. 5 - INCOMPLETO
+
+// Ex. 6
+
+// function sumOfAll(number) {
+//     let counter = [];
+//     let sum = 0;
+//     for (let i = 1; i <= number; i += 1) {
+//         counter.push(i);
+//     }
+//     for (let j in counter) {
+//         sum += counter[j];
+//     }
+//     return sum
+// }
+// console.log(sumOfAll(5))
+
+// Ex. 7
+
+// function verifyEndOfWord(word, ending) {
+
+//     let wordReverse = word.split('').reverse().join('');
+//     let endingReverse = ending.split('').reverse().join('');
+//     let result;
+
+//     for (let i = 0; i < endingReverse.length; i += 1) {
+//         if (wordReverse[i] !== endingReverse[i]) {
+//             result = false;
+//             break;
+//         } else {
+//             result = true;
+//         }
+//     }
+//     return result;
+// }
+// console.log(verifyEndOfWord("trybe", "be"));
