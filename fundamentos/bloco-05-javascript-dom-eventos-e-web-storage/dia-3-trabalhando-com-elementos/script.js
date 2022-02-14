3d67359888392c197a38d2a5652412509e876dc5
Pedro Caldas <pedronerislc@gmail.com>
2022-02-14 19:57:12 -0300

    Add exercise bonus - not done yet

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-trabalhando-com-elementos/script.js
@@ -187,5 +187,34 @@ function changeDayColor() {
 
 changeDayColor();
 
-// Ex. Bonus
+// Ex. Bonus (not done);
 
+// function addNewTask() {
+//   let getInputField = document.querySelector('#task-input');
+//   let addInputButton = document.querySelector('#btn-add');
+//   let getTaskList = document.querySelector('.task-list');
+
+//   addInputButton.addEventListener('click', function() {
+//     if (getInputField.value.length > 0) {
+//       let newLi = document.createElement('li');
+//       newLi.innerText = getInputField.value;
+
+//       getTaskList.appendChild(newLi);
+//       getInputField.value = '';
+//     } else {
+//       alert('Error: Digite ao menos 1 caractere.');
+//     }
+//   })
+
+//   getInputField.addEventListener('keyup', function(event) {
+//     if (event.key === 'Enter' && getInputField.value.length > 0) {
+//       let newLi = document.createElement('li');
+//       newLi.innerText = getInputField.value;
+
+//       getTaskList.appendChild(newLi);
+//       getInputField.value = '';
+//     }
+//   });
+// };
+
+// addNewTask();
