e8a72059530f02e1c0b4140247017faeb6bbf3b5
Pedro Caldas <pedronerislc@gmail.com>
2022-02-14 22:05:57 -0300

    Add exercises from day 5.4

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-web-storage/script.js
@@ -0,0 +1,11 @@
+function changeBackgroundColor() {
+    let body = document.querySelector('body');
+    let bodyColor = body.style.backgroundColor;
+    let getBackgroundBtn = document.getElementById('background-btn');
+
+
+
+
+
+
+}
