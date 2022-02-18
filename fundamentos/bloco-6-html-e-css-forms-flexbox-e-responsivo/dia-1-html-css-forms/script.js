606e89d1bfe60665fe53977b4c6e64a5bb9b113c
Pedro Caldas <pedronerislc@gmail.com>
2022-02-18 17:55:10 -0300

    Add bonus exercise 1

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,5 +1,9 @@
 let submitBtn = document.getElementById('submit-btn');
+let photosCheck = document.getElementById('fotos-checkbox')
 
 submitBtn.addEventListener('click', event => {
+    if (photosCheck = false) {
     event.preventDefault();
+    }
 })
+
