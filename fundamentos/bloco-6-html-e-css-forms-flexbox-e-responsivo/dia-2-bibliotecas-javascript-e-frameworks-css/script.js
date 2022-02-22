df162f97a2992f9edd79fa2db1b5e28e3c4b8011
Pedro Caldas <pedronerislc@gmail.com>
2022-02-22 12:15:00 -0300

    Add libraries

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -1,19 +1,22 @@
-let submitBtn = document.getElementById('submit-btn');
-let photosCheck = document.getElementById('fotos-checkbox');
-let nameInput = document.getElementById('name-input');
-let emailInput = document.getElementById('email-input');
+// let submitBtn = document.getElementById('submit-btn');
+// let photosCheck = document.getElementById('fotos-checkbox');
+// let nameInput = document.getElementById('name-input');
+// let emailInput = document.getElementById('email-input');
 
-submitBtn.addEventListener('click', event => {
-    if (photosCheck = false) {
-    event.preventDefault();
-    }
+// Commenting to test Just-Validate features
+
+// submitBtn.addEventListener('click', event => {
+//     if (photosCheck = false) {
+//     event.preventDefault();
+//     }
     
-    if (nameInput.value.length < 10 || emailInput.value.length < 10) {
-        alert('Dados Inválidos');
-    } else {
-        alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip')
-    }
+//     if (nameInput.value.length < 10 || emailInput.value.length < 10) {
+//         alert('Dados Inválidos');
+//     } else {
+//         alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip')
+//     }
 
     
-})
+// })
+
 
