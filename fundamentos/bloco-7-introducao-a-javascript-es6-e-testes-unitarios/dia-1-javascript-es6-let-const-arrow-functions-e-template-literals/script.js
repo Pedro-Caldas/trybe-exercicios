840f058fa1f354afc54ca11e69545210dc7648c8
Pedro Caldas <pedronerislc@gmail.com>
2022-03-03 19:40:52 -0300

    Add exercises from day 7.1 Part 1

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,49 @@
+// PART 1
+
+// Ex. 1
+
+// - Original Code - 
+
+// function testingScope(escopo) {
+//     if (escopo === true) {
+//       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//       console.log(ifScope);
+//     } else {
+//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+//   }
+
+//   testingScope(true);
+
+// --
+
+// - Refactored Code - 
+
+// const testingScope = (escopo) => {
+//     if (escopo === true) {
+//       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//       console.log(ifScope);
+//     } else {
+//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+//   }
+
+//   testingScope(true);
+
+
+// Ex. 2
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sorter = array => {
+    const sortedArray = array.sort((a, b) => a - b)
+    return sortedArray
+}
+
+console.log(sorter(oddsAndEvens)); // será necessário alterar essa linha
