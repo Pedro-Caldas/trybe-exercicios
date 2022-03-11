aefd43d3e3de6d1c9f05728ac872ca2754fc41ae
Pedro Caldas <pedronerislc@gmail.com>
2022-03-11 11:38:01 -0300

    Add exercise 6 and fix exercise 5 again

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
@@ -1,66 +1,66 @@
 const books = [
-    {
-      id: 1,
-      name: 'As Crônicas de Gelo e Fogo',
-      genre: 'Fantasia',
-      author: {
-        name: 'George R. R. Martin',
-        birthYear: 1948,
-      },
-      releaseYear: 1991,
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
     },
-    {
-      id: 2,
-      name: 'O Senhor dos Anéis',
-      genre: 'Fantasia',
-      author: {
-        name: 'J. R. R. Tolkien',
-        birthYear: 1892,
-      },
-      releaseYear: 1954,
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
     },
-    {
-      id: 3,
-      name: 'Fundação',
-      genre: 'Ficção Científica',
-      author: {
-        name: 'Isaac Asimov',
-        birthYear: 1920,
-      },
-      releaseYear: 1951,
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
     },
-    {
-      id: 4,
-      name: 'Duna',
-      genre: 'Ficção Científica',
-      author: {
-        name: 'Frank Herbert',
-        birthYear: 1920,
-      },
-      releaseYear: 1965,
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
     },
-    {
-      id: 5,
-      name: 'A Coisa',
-      genre: 'Terror',
-      author: {
-        name: 'Stephen King',
-        birthYear: 1947,
-      },
-      releaseYear: 1986,
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
     },
-    {
-      id: 6,
-      name: 'O Chamado de Cthulhu',
-      genre: 'Terror',
-      author: {
-        name: 'H. P. Lovecraft',
-        birthYear: 1890,
-      },
-      releaseYear: 1928,
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
     },
-  ];
-  
+    releaseYear: 1928,
+  },
+];
+
 
 
 
@@ -103,7 +103,7 @@ const books = [
 //     name: 'As Crônicas de Gelo e Fogo',
 //     releaseYear: 1991,
 //   };
-  
+
 //   function getNamedBook(arr) {
 //     return arr.find((book) =>  (book.name.length === 26))
 //   }
@@ -157,7 +157,7 @@ const books = [
 //       releaseYear: 1928,
 //     },
 //   ];
-  
+
 //   function booksOrderedByReleaseYearDesc(arr) {
 //     return arr.sort((a, b)  => b.releaseYear - a.releaseYear)
 //   }
@@ -170,10 +170,7 @@ const books = [
 // const expectedResult = false;
 
 // function everyoneWasBornOnSecXX(arr) {
-//     if(arr.every((element) => {element.releaseYear >= 1900})) {
-//         return true
-//     }
-//     return false
+//     return arr.every((element) => {element.releaseYear >= 1900})
 // }
 
 // everyoneWasBornOnSecXX(books)
@@ -188,5 +185,12 @@ const books = [
 // const expectedResult = true;
 
 // function someBookWasReleaseOnThe80s(arr) {
-//   if(arr.some((element) => element.))
+//   return arr.some((element) => element.releaseYear >= 1980 && element.releaseYear < 1990 )
 // }
+
+// console.log(someBookWasReleaseOnThe80s(books))
+
+
+
+
+
