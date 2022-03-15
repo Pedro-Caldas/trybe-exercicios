c049a7165b73c16cf08b8f4b405f405ee2b94548
Pedro Caldas <pedronerislc@gmail.com>
2022-03-15 19:15:26 -0300

    Add exercise 4

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/script.js
@@ -28,23 +28,69 @@
 
 // Ex. 3
 
-const alex = {
-    name: 'Alex',
-    age: 26,
-    likes: ['fly fishing'],
-    nationality: 'Australian',
-  };
-  
-  const gunnar = {
-    name: 'Gunnar',
-    age: 30,
-    likes: ['hiking', 'scuba diving', 'taking pictures'],
-    nationality: 'Icelandic',
-  };
-  
-  // complete a assinatura da função abaixo
-  const personLikes = ({ name, age, likes } = person) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
-  
-  console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
-  console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+// const alex = {
+//     name: 'Alex',
+//     age: 26,
+//     likes: ['fly fishing'],
+//     nationality: 'Australian',
+//   };
 
+//   const gunnar = {
+//     name: 'Gunnar',
+//     age: 30,
+//     likes: ['hiking', 'scuba diving', 'taking pictures'],
+//     nationality: 'Icelandic',
+//   };
+
+//   // complete a assinatura da função abaixo
+//   const personLikes = ({ name, age, likes } = person) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+//   console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+//   console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+
+
+
+
+// Ex. 4
+
+const people = [
+    {
+        name: 'Nicole',
+        bornIn: 1992,
+        nationality: 'Australian',
+    },
+    {
+        name: 'Harry',
+        bornIn: 2008,
+        nationality: 'Australian',
+    },
+    {
+        name: 'Toby',
+        bornIn: 1901,
+        nationality: 'Australian',
+    },
+    {
+        name: 'Frida',
+        bornIn: 1960,
+        nationality: 'Dannish',
+    },
+    {
+        name: 'Fernando',
+        bornIn: 2001,
+        nationality: 'Brazilian',
+    },
+];
+
+// escreva filterPeople abaixo
+
+//   function filterPeople(arr) {
+//       const filteredArr = arr.filter((element) => element.bornIn < 2000 && element.nationality === 'Australian');
+//       return filteredArr;
+//   }
+
+function filterPeople(people) {
+    const { bornIn, nationality } = people;
+    return people.filter(({bornIn, nationality}) => bornIn < 2000 && nationality === 'Australian')
+}
+
+console.log(filterPeople(people))
